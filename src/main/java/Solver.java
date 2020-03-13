9d8dcf306a643f7648bf7c8bdb021f3671da929b
Jordan Muniz <munizj21@trinityprep.org>
2020-03-12 23:20:46 -0400

    feat: Implement solver methods -doesn't work

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -1,22 +1,127 @@
-public class Solver {
-    public Solver(Board initial) {
-      // find a solution to the initial board (using the A* algorithm)
+import edu.princeton.cs.algs4.MinPQ;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+public class Solver {
+    private class BoardNode implements Comparable<BoardNode> {
+        private int gvalue;
+        private int numOfmoves;
+        private Board current;
+        private BoardNode prev;
+        private boolean isTwin;
+
+        public BoardNode(Board c, BoardNode p, boolean iT, int moves) {
+            if (c == null) throw new NullPointerException("Tried to pass null to BoardNode class");
+
+            current = c;
+
+            prev = p;
+            isTwin = iT;
+            numOfmoves = moves;
+            gvalue = c.manhattan() + c.hamming();
+        }
+
+        @Override
+        public int compareTo(BoardNode boardNode) {
+            if (gvalue > boardNode.gvalue) {
+                return 1;
+            } else if (gvalue < boardNode.gvalue) {
+                return -1;
+            } else {
+                return 0;
+            }
+
+        }
+
+        public String toString() {
+            return current.toString();
+        }
     }
+
+    private Deque<Board> solution;
+    private MinPQ<BoardNode> queue;
+    private BoardNode goalNode;
+    private int nummoves;
+
+    public Solver(Board initial) {
+        // find a solution to the initial board (using the A* algorithm)
+        // initialize the queues with correct comparator(start with hamming)(final should be manhattan)
+        if (initial == null) throw new IllegalArgumentException();
+        queue = new MinPQ<>();
+        solution = new ArrayDeque<>();
+        queue.insert(new BoardNode(initial, null, true, 0));
+        queue.insert(new BoardNode(initial.twin(), null, true, 0));
+        nummoves = getGoalNode();
+    }
+
+    private int getGoalNode() {
+        while (true) {
+            BoardNode deqnode = queue.delMin();
+            if (deqnode.current.isGoal()) {
+                if (deqnode.isTwin) {
+                    goalNode = null;
+                    return -1;
+                } else {
+                    goalNode = deqnode;
+                    return deqnode.numOfmoves;
+                }
+            }
+            if (deqnode.isTwin) {
+                for (Board board : deqnode.current.twin().neighbors()) {
+                    queue.insert(new BoardNode(board, deqnode, true, deqnode.numOfmoves++));
+                }
+            } else {
+                for (Board board : deqnode.current.neighbors()) {
+                    queue.insert(new BoardNode(board, deqnode, false, deqnode.numOfmoves++));
+                }
+
+            }
+        }
+        // MAKE SURe your number of moves is not just recording the iterations in which you dequeue
+
+        // while your queue is not empty
+        // Dequeu from queue
+        // check if its the goal state(if true, return the dequed BN)
+        // add it's neighbors to the queue
+        // create BoardNodes for each neighbor
+        //(current: the nieghbor, prev: dequed BN(Board node), isTwin: dequeed BN, gValue:
+        // important to grab the number of moves from your dequeued node to calculate the current num of moves
+
+
+        // know if it's a twin or not
+        // previous for neighbors is board you came from, to get a solution, keep calling previous until it is null.
+        // repeat for twinQueue( or just stick to one queue)
+        // if you dequeue, and its a twin, and its goal state, then its unsolvable,
+        // if goal and not twin, then solvable.
+    }
+
     public boolean isSolvable() {
-      // is the initial board solvable?
-      return false;
+        return goalNode != null;
     }
+
     public int moves() {
-      // min number of moves to solve initial board; -1 if unsolvable
-        //create a board node class to know where the board came from
-      return 0;
+        // min number of moves to solve initial board; -1 if unsolvable
+        return nummoves;
     }
+
     public Iterable<Board> solution() {
-      // sequence of boards in a shortest solution; null if unsolvable
-      return null;
+        if (goalNode != null) {
+            BoardNode solNode = goalNode;
+            while (solNode.prev != null) { // set a next value in BoardNode class
+                solution.addLast(solNode.current);
+                solNode = solNode.prev;
+            }
+        } else {
+            return null;
+        }
+        // sequence of boards in a shortest solution; null if unsolvable
+
+        return solution;
     }
+
     public static void main(String[] args) {
-      // solve a slider puzzle (given below)
+        // solve a slider puzzle (given below)
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -9,45 +9,51 @@ import org.junit.Test;
 import edu.princeton.cs.algs4.In;
 
 public class BoardTest {
-  
-  Board board;
-  @Before
-  public void setUp() throws Exception {
-    board = generateBoard("input10.txt");
-  }
-  
-  private Board generateBoard(String filename) {
- // create initial board from file
-    In in = new In("8puzzle-test-files/" + filename); // name of file is the min number of moves needed to do it
-    int n = in.readInt();
-    int[][] blocks = new int[n][n];
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            blocks[i][j] = in.readInt();
-    Board initial = new Board(blocks);
 
-    // solve the puzzle
-    Solver solver = new Solver(initial);
+    Board board;
 
-    // print solution to standard output
-//    if (!solver.isSolvable())
-//        StdOut.println("No solution possible");
-//    else {
-//        StdOut.println("Minimum number of moves = " + solver.moves());
-//        for (Board board : solver.solution())
-//            StdOut.println(board);
-//    }
-    return initial;
-  }
+    @Before
+    public void setUp() throws Exception {
+        board = generateBoard("puzzle01.txt");
+    }
 
-  @Test
-  public void testDimension() {
-    //fail("Not yet implemented");
-  }
+    private Board generateBoard(String filename) {
+        // create initial board from file
+        In in = new In("8puzzle-test-files/" + filename); // name of file is the min number of moves needed to do it
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
 
-  @Test
-  public void testSegments() {
-    //fail("Not yet implemented");
-  }
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            System.out.println("No solution possible");
+        else {
+            System.out.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                System.out.println(board);
+        }
+        return initial;
+    }
+
+    @Test
+    public void testDimension() {
+        //fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSolve() {
+
+    }
+
+    @Test
+    public void testSegments() {
+        //fail("Not yet implemented");
+    }
 
 }
